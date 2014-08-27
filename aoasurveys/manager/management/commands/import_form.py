9f466c165e1ffe6b34707d57dd218db14ad0c684
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-08-27 17:03:15 +0300

    First version of parser

--- aoasurveys/manager/management/commands/import_form.py
+++ aoasurveys/manager/management/commands/import_form.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
-from pprint import pprint
+from forms_builder.forms.models import Form, Field, FormEntry, FieldEntry
+from forms_builder.forms.fields import *
 import json
 
 
@@ -7,15 +8,63 @@ class Command(BaseCommand):
     args = '<filename>'
     help = 'Import a dynamic form exported from Naaya-Survey'
 
-    def _parseForm(self, form):
-        #TODO parse form
-        for answer in form["answers"]:
-            #TODO parse answers
-            self._parseAnswer(answer)
+    def _parseForm(self, data):
+        if "title" in data and "questions" in data and "labels" in data:
+            form = Form.objects.create(title=data["title"])
+            for question in data["questions"]:
+                params = {"label": question["title"],
+                          "slug": question["slug"],
+                          "order": question["sortorder"],
+                          "required": question["required"],
+                          "form": form}
 
-    def _parseAnswer(self, answer):
-        #TODO parse answer
-        pass
+                if question["type"] == "FileWidget":
+                    #TODO size_max
+                    params["field_type"] = FILE
+
+                if question["type"] == "StringWidget":
+                    #TODO set width, size_max
+                    params["field_type"] = TEXT
+
+                if question["type"] == "CheckboxesWidget":
+                    params["visible"] = question["display"]
+                    params["choices"] = ",".join(question["choices"])
+                    params["field_type"] = CHECKBOX_MULTIPLE
+
+                if question["type"] == "RadioWidget":
+                    #TODO set add_extra_choice
+                    params["visible"] = question["display"]
+                    params["choices"] = ",".join(question["choices"])
+                    params["field_type"] = RADIO_MULTIPLE
+
+                if question["type"] == "TextAreaWidget":
+                    #TODO set columns, rows
+                    params["field_type"] = TEXTAREA
+
+                if question["type"] == "ComboboxWidget":
+                    params["choices"] = ",".join(question["choices"])
+                    params["field_type"] = SELECT
+
+                if question["type"] == "CheckboxMatrixWidget":
+                    params["choices"] = ",".join(question["choices"])
+                    params["field_type"] = CHECKBOX_MULTIPLE
+
+                if question["type"] == "LocalizedTextAreaWidget":
+                    #TODO set columns, rows
+                    params["field_type"] = TEXTAREA
+
+                if question["type"] == "GeoWidget":
+                    params["field_type"] = TEXT
+
+                if question["type"] == "LocalizedStringWidget":
+                    #TODO set width, size_max
+                    params["field_type"] = TEXTAREA
+
+                Field.objects.create(**params)
+
+        else:
+            self.stdout.write('JSON is not in expected format.')
+            return
 
     def handle(self, *args, **options):
         if not args:
@@ -29,11 +78,6 @@ class Command(BaseCommand):
             self.stdout.write('File is not in JSON format.')
             return
 
-        if "forms" in data:
-            for form in data["forms"]:
-                self._parseForm(form)
-        else:
-            self.stdout.write('JSON is not in format expected.')
-            return
+        self._parseForm(data)
 
         datafile.close()
