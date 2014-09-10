2f58bef547fea3c64919c877514dc005e04762e4
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-09-10 15:35:37 +0300

    [forms] enable form status management.

--- aoasurveys/reports/forms.py
+++ aoasurveys/reports/forms.py
@@ -1,9 +1,15 @@
-from django.forms import Form, CharField, MultipleChoiceField
+from django.forms import (
+    Form, CharField, MultipleChoiceField, ChoiceField, RadioSelect, TextInput,
+)
+from forms_builder.forms.models import STATUS_CHOICES
 
 
 class SelectFieldsForm(Form):
-    visible_fields = CharField(max_length=255, required=False)
-    filtering_fields = CharField(max_length=255, required=False)
+    visible_fields = CharField(max_length=255, required=False,
+                               widget=TextInput(attrs={'size': 40}))
+    filtering_fields = CharField(max_length=255, required=False,
+                                 widget=TextInput(attrs={'size': 40}))
+    status = ChoiceField(choices=STATUS_CHOICES, widget=RadioSelect)
 
 
 class FilteringForm(Form):
