12b68340a2d2401fada3a226ba4ca7a014a08bec
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-09-19 17:31:42 +0300

    [utils] use default language where current language is not available.

--- aoasurveys/reports/utils.py
+++ aoasurveys/reports/utils.py
@@ -3,7 +3,10 @@ from django.conf import settings
 
 def get_translation(value, language=settings.DEFAULT_LANGUAGE):
     translations = value.split('\n') if value else []
-    if len(translations) == 2:
-        return translations[settings.LOCALIZED_LANGUAGES_ABBR.index(language)]
-    else:
-        return value
+    if len(translations) == len(settings.LOCALIZED_LANGUAGES_ABBR):
+        return (
+            translations[settings.LOCALIZED_LANGUAGES_ABBR.index(language)] or
+            translations[settings.LOCALIZED_LANGUAGES_ABBR
+                         .index(settings.DEFAULT_LANGUAGE)]
+        )
+    return value
