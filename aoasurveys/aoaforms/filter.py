7892ae9f0ae351497216bda26de73b1243e10355
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-09-25 15:26:04 +0300

    [reports] Set count on RawQuerySet.

--- aoasurveys/aoaforms/filter.py
+++ aoasurveys/aoaforms/filter.py
@@ -1,19 +1,21 @@
+from django.db import connection
+
 from aoasurveys.aoaforms.models import FormEntry
 
 
 def filter_entries(form, filters):
-    query = "SELECT f.entry_id AS id FROM aoaforms_fieldentry f "
+    query = "SELECT f.id FROM aoaforms_formentry f "
     index = 0
     for key, value in filters.iteritems():
         query += (
-            "JOIN aoaforms_fieldentry f{index} ON f{index}.entry_id=f.entry_id "
+            "JOIN aoaforms_fieldentry f{index} ON f{index}.entry_id=f.id "
             "AND f{index}.field_id='{key}' "
         )
         if isinstance(value, list):
             query += "AND ("
             query += " OR ".join([
                 "f{index}.value LIKE '%%{choice}%%'".format(index=index,
-                                                          choice=choice)
+                                                            choice=choice)
                 for choice in value])
             query += ") "
         else:
@@ -21,12 +23,19 @@ def filter_entries(form, filters):
 
         query = query.format(index=index, key=key, value=value)
         index += 1
-    query += "GROUP BY f.entry_id"
+    query += "GROUP BY f.id"
 
     if filters:
         raw = FormEntry.objects.raw(query)
-        raw.count = lambda: len(list(raw))
-        raw.order_by = lambda b: raw # FIXME: actual ordering
+        cursor = connection.cursor()
+        cursor.execute(
+            query
+            .replace('f.id', 'COUNT(f.id)', 1)
+            .replace('GROUP BY f.id', '')
+        )
+        count = cursor.fetchone()[0]
+        raw.count = lambda: count
+        raw.order_by = lambda b: raw  # FIXME: actual ordering
         return raw
 
     return form.entries.all()
