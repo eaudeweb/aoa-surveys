cc5fc265ab0feece251465da083df11e6d5317cf
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-10-17 18:58:07 +0300

    [manager] Split management view in two: form properties and form fields+labels.

--- aoasurveys/manager/urls.py
+++ aoasurveys/manager/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 
-from aoasurveys.manager.views import FormManagementView
+from aoasurveys.manager.views import FormPropertiesView, FormFieldsView
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>.*)/$', FormManagementView.as_view(), name='manage_form'),
+    url(r'^(?P<slug>.*)/properties/$', FormPropertiesView.as_view(),
+        name='manage_properties'),
+    url(r'^(?P<slug>.*)/fields/$', FormFieldsView.as_view(),
+        name='manage_fields'),
 )
--- aoasurveys/manager/views.py
+++ aoasurveys/manager/views.py
@@ -7,7 +7,6 @@ from aoasurveys.manager.forms import SelectFieldsForm
 
 
 class FormManagementView(DetailView, FormView):
-    template_name = 'manage_form.html'
     model = Form
     slug_url_kwarg = 'slug'
     form_class = SelectFieldsForm
@@ -20,19 +19,52 @@ class FormManagementView(DetailView, FormView):
         context = self.get_context_data(object=self.object, form=form)
         return self.render_to_response(context)
 
-    def get_initial(self):
-        survey = self.get_object()
-        return {
-            'status': survey.status,
-            'visible_fields': survey.visible_fields_slugs,
-            'filtering_fields': survey.filtering_fields_slugs,
-        }
-
     def get_success_url(self):
         return reverse('homepage')
 
     def get_context_data(self, **kwargs):
         context = super(FormManagementView, self).get_context_data(**kwargs)
+        context['tab'] = self.tab
+        return context
+
+
+class FormPropertiesView(FormManagementView):
+    template_name = 'properties.html'
+    tab = 'properties'
+
+    def get_initial(self):
+        survey = self.get_object()
+        return {
+            'status': survey.status,
+        }
+
+    def form_valid(self, form):
+        survey = self.get_object()
+        survey.status = form.cleaned_data['status']
+        survey.save()
+        return super(FormPropertiesView, self).form_valid(form)
+
+
+class FormFieldsView(FormManagementView):
+    template_name = 'fields.html'
+    tab = 'fields'
+
+    def get_initial(self):
+        survey = self.get_object()
+        return {
+            'visible_fields': survey.visible_fields_slugs,
+            'filtering_fields': survey.filtering_fields_slugs,
+        }
+
+    def form_valid(self, form):
+        survey = self.get_object()
+        survey.visible_fields_slugs = form.cleaned_data['visible_fields']
+        survey.filtering_fields_slugs = form.cleaned_data['filtering_fields']
+        survey.save()
+        return super(FormFieldsView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(FormFieldsView, self).get_context_data(**kwargs)
 
         form = context['object']
         fields_and_labels = list(form.fields.all()) + list(form.labels.all())
@@ -43,11 +75,3 @@ class FormManagementView(DetailView, FormView):
             'separator': settings.FIELDS_SEPARATOR,
         })
         return context
-
-    def form_valid(self, form):
-        survey = self.get_object()
-        survey.status = form.cleaned_data['status']
-        survey.visible_fields_slugs = form.cleaned_data['visible_fields']
-        survey.filtering_fields_slugs = form.cleaned_data['filtering_fields']
-        survey.save()
-        return super(FormManagementView, self).form_valid(form)
