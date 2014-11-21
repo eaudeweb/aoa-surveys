790ac21de082930151c8fb28a45c99679222b228
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-11-21 18:41:08 +0200

    [forms] Implement new survey creation.

--- aoasurveys/aoaforms/urls.py
+++ aoasurveys/aoaforms/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
-from aoasurveys.aoaforms.views import FormSent, FormView
+from aoasurveys.aoaforms.views import (
+    FormSent, FormView, CreateSurvey
+)
 
 
 urlpatterns = patterns('aoasurveys.aoaforms.views',
     url(r"(?P<slug>.*)/sent/$", FormSent.as_view(), name="form_sent"),
     url(r'^(?P<slug>.*)/form/$', FormView.as_view(), name='form_detail'),
+    url(r'^new_form/$', CreateSurvey.as_view(), name='new_form'),
 )
